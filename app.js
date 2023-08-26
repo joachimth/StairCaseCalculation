bf6f694ab71278e6ca21c6e0bbf399c5925523c6
Joachim Thirsbro <joachim@thirsbro.dk>
2023-08-26 22:50:32 +0200

    app.js rep

--- app.js
+++ app.js
@@ -1,121 +1,115 @@
-document.getElementById('calculateBtn').addEventListener('click', updateStaircase);
-
-function updateStaircase() {
-    const totalHeight = parseFloat(document.getElementById('totalHeight').value);
-    const totalDepth = parseFloat(document.getElementById('totalDepth').value);
-
-    const MIN_STEP_HEIGHT = 15; // cm
-    const MAX_STEP_HEIGHT = 25; // cm
-    const MIN_STEP_DEPTH = 20; // cm
-    const MAX_STEP_DEPTH = 50; // cm
-
-    let optimalStepHeight = (MIN_STEP_HEIGHT + MAX_STEP_HEIGHT) / 2;
-    let optimalStepDepth = (MIN_STEP_DEPTH + MAX_STEP_DEPTH) / 2;
-
-    const numSteps = Math.round(totalHeight / optimalStepHeight);
-
-    optimalStepHeight = totalHeight / numSteps;
-    optimalStepDepth = totalDepth / (numSteps - 1);
-
-    document.getElementById('numSteps').innerText = numSteps;
-    document.getElementById('stepHeight').innerText = optimalStepHeight.toFixed(2);
-    document.getElementById('stepDepth').innerText = optimalStepDepth.toFixed(2);
-
-    drawStaircase(totalHeight, totalDepth, optimalStepHeight, optimalStepDepth);
-    drawSingleStep(optimalStepHeight, optimalStepDepth);
-}
+const drawingArea = document.querySelector('#drawingArea');
+const svgns = "http://www.w3.org/2000/svg";
 
 function drawStaircase(steps, stepHeight, stepDepth) {
-    // Select the SVG container and clear any previous drawing
-    let svgContainer = d3.select("#staircaseDiagram");
-    svgContainer.selectAll("*").remove();
+    // Clear previous drawings
+    drawingArea.innerHTML = "";
 
-    // Dimensions
-    let width = 400;
-    let height = steps * stepHeight + 50; // Added +50 for some margin
+    const svgContainer = d3.select("#drawingArea").append("svg")
+        .attr("width", 300)
+        .attr("height", 300);
 
-    svgContainer.attr("width", width).attr("height", height);
+    // Add shadow filter
+    let defs = svgContainer.append("defs");
+    let filter = defs.append("filter")
+        .attr("id", "shadow")
+        .attr("height", "130%");
 
-    // Create steps
+    filter.append("feOffset")
+        .attr("dx", "2")
+        .attr("dy", "2")
+        .attr("result", "offOut");
+
+    filter.append("feGaussianBlur")
+        .attr("in", "offOut")
+        .attr("stdDeviation", "2")
+        .attr("result", "blurOut");
+
+    filter.append("feBlend")
+        .attr("in", "SourceGraphic")
+        .attr("in2", "blurOut")
+        .attr("mode", "normal");
+
+    // Create steps with shadow effect
     for (let i = 0; i < steps; i++) {
         svgContainer.append("rect")
             .attr("x", 0)
-            .attr("y", i * stepHeight) // This should place steps above each other
+            .attr("y", i * stepHeight)
             .attr("width", stepDepth)
             .attr("height", stepHeight)
             .style("fill", "#888")
-            .style("stroke", "#333");
+            .style("stroke", "#333")
+            .style("filter", "url(#shadow)"); // Apply shadow here
     }
 
-    // Draw ground and top floor levels
+    // Drawing the base level
     svgContainer.append("line")
         .attr("x1", 0)
         .attr("y1", 0)
-        .attr("x2", width)
+        .attr("x2", 300)
         .attr("y2", 0)
-        .style("stroke", "#333")
-        .style("stroke-width", 3);
+        .attr("stroke", "black")
+        .attr("stroke-width", 2);
 
+    // Drawing the top level
     svgContainer.append("line")
         .attr("x1", 0)
-        .attr("y1", height - 50)
-        .attr("x2", width)
-        .attr("y2", height - 50)
-        .style("stroke", "#333")
-        .style("stroke-width", 3);
+        .attr("y1", steps * stepHeight)
+        .attr("x2", 300)
+        .attr("y2", steps * stepHeight)
+        .attr("stroke", "black")
+        .attr("stroke-width", 2);
 }
-
 
 function drawSingleStep(stepHeight, stepDepth) {
-    d3.select("#singleStepDiagram").html("");
+    // Clear previous drawings
+    drawingArea.innerHTML = "";
 
-    const stepWidth = 300;
-    const svgHeight = stepHeight * 10 + 50;
-    const svgDepth = stepDepth * 10 + 50;
+    const svgContainer = d3.select("#drawingArea").append("svg")
+        .attr("width", 300)
+        .attr("height", 300);
 
-    const svg = d3.select("#singleStepDiagram")
-                  .append("svg")
-                  .attr("width", stepWidth)
-                  .attr("height", svgHeight + svgDepth);
+    // Add shadow filter (same as above)
+    let defs = svgContainer.append("defs");
+    let filter = defs.append("filter")
+        .attr("id", "shadow")
+        .attr("height", "130%");
 
-    svg.append("rect")
-       .attr("x", 50)
-       .attr("y", 50)
-       .attr("width", stepDepth * 10)
-       .attr("height", stepHeight * 10)
-       .attr("fill", "#ddd");
+    filter.append("feOffset")
+        .attr("dx", "2")
+        .attr("dy", "2")
+        .attr("result", "offOut");
 
-    svg.append("line")
-       .attr("x1", 30)
-       .attr("y1", 50)
-       .attr("x2", 30)
-       .attr("y2", 50 + stepHeight * 10)
-       .attr("stroke", "black")
-       .attr("stroke-width", 2)
-       .attr("marker-start", "url(#arrowhead)")
-       .attr("marker-end", "url(#arrowhead)");
+    filter.append("feGaussianBlur")
+        .attr("in", "offOut")
+        .attr("stdDeviation", "2")
+        .attr("result", "blurOut");
 
-    svg.append("text")
-       .attr("x", 10)
-       .attr("y", 50 + (stepHeight * 10 / 2))
-       .attr("text-anchor", "middle")
-       .attr("alignment-baseline", "middle")
-       .text(`${stepHeight.toFixed(2)} cm`);
+    filter.append("feBlend")
+        .attr("in", "SourceGraphic")
+        .attr("in2", "blurOut")
+        .attr("mode", "normal");
 
-    svg.append("line")
-       .attr("x1", 50)
-       .attr("y1", svgHeight + 30)
-       .attr("x2", 50 + stepDepth * 10)
-       .attr("y2", svgHeight + 30)
-       .attr("stroke", "black")
-       .attr("stroke-width", 2)
-       .attr("marker-start", "url(#arrowhead)")
-       .attr("marker-end", "url(#arrowhead)");
+    // Drawing the step with shadow effect
+    svgContainer.append("rect")
+        .attr("x", 50)
+        .attr("y", 100)
+        .attr("width", stepDepth)
+        .attr("height", stepHeight)
+        .style("fill", "#888")
+        .style("stroke", "#333")
+        .style("filter", "url(#shadow)");
 
-    svg.append("text")
-       .attr("x", 50 + (stepDepth * 10 / 2))
-       .attr("y", svgHeight + 40)
-       .attr("text-anchor", "middle")
-       .attr("alignment-baseline", "middle")
-       .text(`${stepDepth.toFixed(2)} cm`);
+    // Add measurements and labels...
+    // [Code for adding measurement lines, text, etc.]
 }
+
+// Event listeners for input changes
+document.querySelector('#heightDifference').addEventListener('input', calculateAndDraw);
+document.querySelector('#maxDepth').addEventListener('input', calculateAndDraw);
+
+function calculateAndDraw() {
+    // ... [Rest of the function as provided before]
+}
+
+calculateAndDraw();  // Initial drawing
